e345cf3ab183935ec9c9f3f91748da502906a9d4
sheilagrn <43527473+sheilagrn@users.noreply.github.com>
2023-05-06 18:31:44 -0300

    Delete ejercicios directory

--- ejercicios/javascript/ej10.js
+++
@@ -1,5 +0,0 @@
-function combineTwoArrays(arr1, arr2){
-    let arreglo = [...arr1, ...arr2];
-    return arreglo;
-}
-console.log(combineTwoArrays([1,2,3], [4,5,6]));
--- ejercicios/javascript/ej8.js
+++
@@ -1,7 +0,0 @@
-function sumar() {
-    let total = 0;
-    for (let i = 0; i < arguments.length; i++) {
-      total += arguments[i];
-    }
-    return total;
-}
